35ef1ec3bff897ef0ca43ba46c352fe1debdc7d0
avizaguri-macbook <avi210892@gmail.com>
2025-06-20 19:51:33 +0300

    Bump package version to v0.1.3 and add initial test files and configuration

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = '0.1.2.1'
+package_version = '0.1.3'
 
 package_name = 'pytest-plugins'
 package_description = 'A Python package for managing pytest plugins.'
