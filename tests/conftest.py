1d0ff283101c1fc4d12826c399ba22fd61171e55
avizaguri-macbook <avi210892@gmail.com>
2025-06-21 13:48:43 +0300

    Add conftest.py for pytest plugin configuration and create initial test_dummy.py with sample tests

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+pytest_plugins = [
+    "pytest_plugins.better_report",
+    "pytest_plugins.max_fail_streak",
+    "pytest_plugins.fail2skip",
+    "pytest_plugins.add_config_parameters",
+]
---
+++ tests/test_dummy.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+class TestDummy:
+    def test_pass(self):
+        assert True
+
+    def test_false(self):
+        assert False
+
+    @pytest.mark.fail2skip
+    @pytest.mark.parametrize("param1, param2", [(1, 1), (2, 2), (3, 3), (4, 5)])
+    def test_with_parameters_1(self, param1, param2):
+        assert param1 != param2
+
+    @pytest.mark.parametrize("param1, param2", [(1, 1), (2, 2), (3, 3), (4, 5)])
+    def test_with_parameters_2(self, param1, param2):
+        assert param1 != param2
