2eafd6d2b07c02e4c87804df59746fdddf42b6fb
avizaguri-macbook <avi210892@gmail.com>
2025-09-14 21:04:37 +0300

    add build_logger to tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from custom_python_logger import build_logger
+
 pytest_plugins = [
     "pytest_plugins.better_report",
     "pytest_plugins.max_fail_streak",
@@ -5,3 +7,8 @@ pytest_plugins = [
     "pytest_plugins.add_config_parameters",
     "pytest_plugins.verbose_param_ids",
 ]
+
+logger = build_logger(
+    project_name="pytest-plugins",
+    log_file=True
+)
