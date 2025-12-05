810e50e6338fadc3afdbf83469b3c66154a830ea
avizaguri-macbook <avi210892@gmail.com>
2025-12-05 09:40:44 +0200

    Refactor tests to use pytest's exception handling and remove unused parameters

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,4 @@
 from custom_python_logger import build_logger
 
-# pytest_plugins = [
-#     "pytest_plugins.better_report",
-#     "pytest_plugins.max_fail_streak",
-#     "pytest_plugins.fail2skip",
-#     "pytest_plugins.add_config_parameters",
-#     "pytest_plugins.verbose_param_ids",
-# ]
 
 logger = build_logger(project_name="pytest-plugins", log_file=True)
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,6 @@
 import pytest
 
-parametrize = pytest.mark.parametrize("param1, param2", [("A", "A"), (2, 2), (3, 3), (4, 5)])
+parametrize1 = pytest.mark.parametrize("param1, param2", [("A", "A"), (2, 2), (3, 3)])
 
 
 @pytest.fixture(scope="session")
@@ -26,7 +26,8 @@ def test_unexpected_pass() -> None:
 @pytest.mark.abc
 def test_abc() -> None:
     """A dummy test to ensure pytest is working."""
-    assert False, "This is a dummy test to ensure pytest is working."
+    with pytest.raises(AssertionError):
+        assert False, "This is a dummy test to ensure pytest is working."
 
 
 class TestDummy:
@@ -36,23 +37,26 @@ class TestDummy:
 
     @pytest.mark.test_fail
     def test_false(self) -> None:  # pylint: disable=R6301, W0613,  W0621
-        assert False, {"message": "This test is expected to fail."}  # noqa: B011
+        with pytest.raises(AssertionError):
+            assert False, {"message": "This test is expected to fail."}  # noqa: B011
 
     @pytest.mark.fail2skip(reason="This test is expected to fail and be skipped.")
     def test_fail2skip(self) -> None:  # pylint: disable=R6301, W0613,  W0621
         assert False, {"message": "This test is expected to fail."}  # noqa: B011
 
-    @parametrize
+    @parametrize1
     def test_with_parameters_1(self, param1: str | int, param2: str | int) -> None:  # pylint: disable=R6301,W0613,W0621
-        assert param1 != param2, {
-            "expected_value": param1,
-            "actual_value": param2,
-            "diagnostic_info": {"param1": param1, "param2": param2},
-        }
+        with pytest.raises(AssertionError):
+            assert param1 != param2, {
+                "expected_value": param1,
+                "actual_value": param2,
+                "diagnostic_info": {"param1": param1, "param2": param2},
+            }
 
-    @pytest.mark.parametrize("param1, param2", [(1, 1), (2, 2), (3, 3), (4, 5)])
+    @pytest.mark.parametrize("param1, param2", [(1, 1), (2, 2), (3, 3)])
     def test_with_parameters_2(self, param1: int, param2: int) -> None:  # pylint: disable=R6301, W0613,  W0621
-        assert param1 != param2
+        with pytest.raises(AssertionError):
+            assert param1 != param2
 
 
 def test_dummy_1() -> None:
@@ -81,4 +85,5 @@ class TestDummy3:
         assert True
 
     def test_pass2(self, print_x) -> None:  # pylint: disable=R6301, W0613,  W0621
-        assert False
+        with pytest.raises(AssertionError):
+            assert False
